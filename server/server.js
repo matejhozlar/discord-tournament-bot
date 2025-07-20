ee2d95aaeaa0405fb222543362152a59bb98f0a1
Matej <hozlarmatej0@gmail.com>
2025-07-21 01:27:25 +0200

    server init

---
+++ server/server.js
@@ -0,0 +1,8 @@
+import app from "./app/app.js";
+import dotenv from "dotenv";
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, (req, res) => {
+  console.log("Server started on http://127.0.0.1:5000");
+});
